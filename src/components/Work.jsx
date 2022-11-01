a6b04677911280596702a322a08465fecd58d930
lucy1528 <luxysm@gmail.com>
2022-10-31 23:38:31 -0600

    adding images routes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,27 +4,30 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import data from "../assets/data.json";
 
 const Work = () => {
+  const http = 'https://';
+  const cargarImagen = require.context("../assets", true);
   return (
-    <div id="work">
+    <div id="work">    
       <h2>WORK</h2>
-      <section>
+      <section>            
         <article>
-          <Carousel
+          <Carousel 
             showArrows={false}
             showIndicators={false}
             showStatus={false}
             showThumbs={false}
-            interval={2000}
+            interval={5000}
             infiniteLoop={true}
             autoPlay={true}
           >
             {data.projects.map((i) => (
-              <div key={i.title} className="workItem">
-                <img src={i.imgSrc} alt={i.title} />
+              <div key={i.title} className="workItem">              
+                <img  src={cargarImagen(`./${i.imgSrc}.png`)} alt={i.title} />
+               
                 <aside>
                   <h3>{i.title}</h3>
                   <p>{i.description}</p>
-                  <a target={"blank"} href={i.url}>
+                  <a target={"blank"} href={http+i.url}>                  
                     View Demo
                   </a>
                 </aside>
